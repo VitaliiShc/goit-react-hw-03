940f016c28179027cb7f92aa740b2bd6005f74c1
VitaliiShc <135863695+VitaliiShc@users.noreply.github.com>
2024-02-20 04:33:25 +0200

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+// import css from './App.module.css';
+// import PropTypes from 'prop-types';
+
+export default function App() {
+
+  return (
+    <>
+      <h1>React Hw Template</h1>
+    </>
+  );
+}
